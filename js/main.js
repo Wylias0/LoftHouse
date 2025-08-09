5bb69b80b278406846405e79b6730dfe7e14d0f8
Wylias0 <anatoliy.pantuhov@yandex.ru>
2025-08-09 08:01:11 +0400

    add phone mask

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ navBtn.onclick = function () {
     document.body.classList.toggle('no-scroll')
 }
 
+
+/* Phone Mask */
+
+mask('[data-tel-input]');
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+
+phoneInputs.forEach((input)=> {
+
+    input.addEventListener('input', ()=> {
+    if(input.value == '+') input.value = '';
+})
+    
+    input.addEventListener('blur', ()=> {
+    if(input.value == '+') input.value = '';
+})
+
+});
+
+
